6ca5ce4a57dc225987df7271601c283ec8223b44
Test User <test@example.com>
2025-04-15 20:10:49 +1000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
@@ -13,13 +15,13 @@ export default defineConfig({
   },
   define: {
     __FIREBASE_CONFIG__: {
-      apiKey: process.env.VITE_FIREBASE_API_KEY,
-      authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-      projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.VITE_FIREBASE_APP_ID,
-      measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+      apiKey: JSON.stringify(import.meta.env.VITE_FIREBASE_API_KEY),
+      authDomain: JSON.stringify(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN),
+      projectId: JSON.stringify(import.meta.env.VITE_FIREBASE_PROJECT_ID),
+      storageBucket: JSON.stringify(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET),
+      messagingSenderId: JSON.stringify(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID),
+      appId: JSON.stringify(import.meta.env.VITE_FIREBASE_APP_ID),
+      measurementId: JSON.stringify(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID),
     },
   },
 });
